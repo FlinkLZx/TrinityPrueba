3d34bfcdb659c01e3f78780fa2fe8f5e51317226
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-19 17:17:03 -0500

    Finished ClientesService

--- src/main/java/com/example/TrinityPrueba/service/ClientesService.java
+++ src/main/java/com/example/TrinityPrueba/service/ClientesService.java
@@ -1,4 +1,16 @@
 package com.example.TrinityPrueba.service;
 
+import com.example.TrinityPrueba.entities.Clientes;
+
+import java.util.List;
+
 public interface ClientesService {
+
+    List<Clientes> getClientes();
+
+    void createClientes(Clientes clientes) throws Exception;
+
+    void updateClientes(Clientes clientes);
+
+    void deleteClientes(Clientes clientes);
 }
