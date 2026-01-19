2d2630eb202a34e475a510b8c24e7cebcf960947
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-19 17:37:39 -0500

    Finished ProductosService

--- src/main/java/com/example/TrinityPrueba/service/ProductosService.java
+++ src/main/java/com/example/TrinityPrueba/service/ProductosService.java
@@ -1,4 +1,16 @@
 package com.example.TrinityPrueba.service;
 
+import com.example.TrinityPrueba.entities.Productos;
+
+import java.util.List;
+
 public interface ProductosService {
+
+    List<Productos> getProductos();
+
+    void createProductos(Productos productos) throws Exception;
+
+    void updateProductos(Productos productos) throws Exception;
+
+    void deleteProductos(Productos productos) throws Exception;
 }
