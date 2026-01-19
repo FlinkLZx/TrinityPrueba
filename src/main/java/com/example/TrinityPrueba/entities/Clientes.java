fa835eb90d07429545ceca6e52d2499601bd9f38
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-19 16:07:17 -0500

    Added Clientes values

--- src/main/java/com/example/TrinityPrueba/entities/Clientes.java
+++ src/main/java/com/example/TrinityPrueba/entities/Clientes.java
@@ -1,4 +1,113 @@
 package com.example.TrinityPrueba.entities;
 
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table
 public class Clientes {
+
+    private List<Productos> tipoDeCuenta;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String tipoDeidentificacion;
+
+    private String numeroIdentificacion;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String mail;
+
+    private String nacimiento;
+
+    private String creacion;
+
+    private String modificacion;
+
+    public List<Productos> getTipoDeCuenta() {
+        return tipoDeCuenta;
+    }
+
+    public void setTipoDeCuenta(List<Productos> tipoDeCuenta) {
+        this.tipoDeCuenta = tipoDeCuenta;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTipoDeidentificacion() {
+        return tipoDeidentificacion;
+    }
+
+    public void setTipoDeidentificacion(String tipoDeidentificacion) {
+        this.tipoDeidentificacion = tipoDeidentificacion;
+    }
+
+    public String getNumeroIdentificacion() {
+        return numeroIdentificacion;
+    }
+
+    public void setNumeroIdentificacion(String numeroIdentificacion) {
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(String nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public String getCreacion() {
+        return creacion;
+    }
+
+    public void setCreacion(String creacion) {
+        this.creacion = creacion;
+    }
+
+    public String getModificacion() {
+        return modificacion;
+    }
+
+    public void setModificacion(String modificacion) {
+        this.modificacion = modificacion;
+    }
 }
