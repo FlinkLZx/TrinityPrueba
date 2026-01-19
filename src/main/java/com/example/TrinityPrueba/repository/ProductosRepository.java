cfc189c2927880f32fec2ce96174dcc6d0ef02f5
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-19 16:35:59 -0500

    Modified ProductosRepository

--- src/main/java/com/example/TrinityPrueba/repository/ProductosRepository.java
+++ src/main/java/com/example/TrinityPrueba/repository/ProductosRepository.java
@@ -1,4 +1,9 @@
 package com.example.TrinityPrueba.repository;
 
-public interface ProductosRepository {
+import com.example.TrinityPrueba.entities.Productos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductosRepository extends JpaRepository<Productos, Long> {
 }
