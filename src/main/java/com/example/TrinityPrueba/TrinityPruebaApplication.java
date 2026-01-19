53770d5f52d637a7d7db2d67849026a8086028a2
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-19 15:40:38 -0500

    first commit

---
+++ src/main/java/com/example/TrinityPrueba/TrinityPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrinityPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinityPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinityPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrinityPrueba/TrinityPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrinityPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrinityPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
