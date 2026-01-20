6abd7e8310e6956f22643873c369f358b785dc2f
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-20 08:40:19 -0500

    FIxed ClientesController

--- src/main/java/com/example/TrinityPrueba/controllers/ClientesController.java
+++ src/main/java/com/example/TrinityPrueba/controllers/ClientesController.java
@@ -2,10 +2,7 @@ package com.example.TrinityPrueba.controllers;
 
 import com.example.TrinityPrueba.entities.Clientes;
 import com.example.TrinityPrueba.service.ClientesService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,17 +21,17 @@ public class ClientesController {
         return clientesService.getClientes();
     }
 
-    @GetMapping("/update")
+    @PutMapping("/update")
     public void updateClientes(@RequestBody Clientes clientes) {
         clientesService.updateClientes(clientes);
     }
 
-    @GetMapping("/create")
+    @PostMapping("/create")
     public void createClientes(@RequestBody Clientes clientes) throws Exception {
         clientesService.createClientes(clientes);
     }
 
-    @GetMapping("/delete")
+    @DeleteMapping("/delete")
     public void deleteClientes(Clientes clientes) {
         clientesService.deleteClientes(clientes);
     }
