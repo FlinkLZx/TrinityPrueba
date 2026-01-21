e3fc7ca8b24ad769a034f7d5a1c49f2b2c28b15e
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-21 15:02:33 -0500

    Variables fixed in Clientes

--- src/main/java/com/example/TrinityPrueba/entities/Clientes.java
+++ src/main/java/com/example/TrinityPrueba/entities/Clientes.java
@@ -2,11 +2,14 @@ package com.example.TrinityPrueba.entities;
 
 
 import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table
+@Table(name = "clientes")
 public class Clientes {
 
     //Correcion pendiente
@@ -18,25 +21,21 @@ public class Clientes {
     private Long Id;
 
     private String tipoDeIdentificacion;
-
     private String numeroIdentificacion;
 
     private String nombre;
-
     private String apellido;
 
     private String mail;
 
-    private String nacimiento;
+    private LocalDate nacimiento;
 
-    private String creacion;
-
-    private String modificacion;
+    private LocalDateTime creacion;
+    private LocalDateTime modificacion;
 
     public List<Productos> getTipoDeCuenta() {
         return tipoDeCuenta;
     }
-
     public void setTipoDeCuenta(List<Productos> tipoDeCuenta) {
         this.tipoDeCuenta = tipoDeCuenta;
     }
@@ -44,7 +43,6 @@ public class Clientes {
     public Long getId() {
         return Id;
     }
-
     public void setId(Long id) {
         Id = id;
     }
@@ -52,7 +50,6 @@ public class Clientes {
     public String getTipoDeIdentificacion() {
         return tipoDeIdentificacion;
     }
-
     public void setTipoDeIdentificacion(String tipoDeidentificacion) {
         this.tipoDeIdentificacion = tipoDeidentificacion;
     }
@@ -60,7 +57,6 @@ public class Clientes {
     public String getNumeroIdentificacion() {
         return numeroIdentificacion;
     }
-
     public void setNumeroIdentificacion(String numeroIdentificacion) {
         this.numeroIdentificacion = numeroIdentificacion;
     }
@@ -68,7 +64,6 @@ public class Clientes {
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -76,7 +71,6 @@ public class Clientes {
     public String getApellido() {
         return apellido;
     }
-
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
@@ -84,32 +78,28 @@ public class Clientes {
     public String getMail() {
         return mail;
     }
-
     public void setMail(String mail) {
         this.mail = mail;
     }
 
-    public String getNacimiento() {
-        return nacimiento;
+    public LocalDate getNacimiento() {
+        return this.nacimiento;
     }
-
-    public void setNacimiento(String nacimiento) {
+    public void setNacimiento(LocalDate nacimiento) {
         this.nacimiento = nacimiento;
     }
 
-    public String getCreacion() {
+    public LocalDateTime getCreacion() {
         return creacion;
     }
-
-    public void setCreacion(String creacion) {
+    public void setCreacion(LocalDateTime creacion) {
         this.creacion = creacion;
     }
 
-    public String getModificacion() {
+    public LocalDateTime getModificacion() {
         return modificacion;
     }
-
-    public void setModificacion(String modificacion) {
+    public void setModificacion(LocalDateTime modificacion) {
         this.modificacion = modificacion;
     }
 }
