3da7087567d4ef6f9a890be6ed0c05943d7c54e5
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-21 16:02:03 -0500

    Modified ClientesController

--- src/main/java/com/example/TrinityPrueba/controllers/ClientesController.java
+++ src/main/java/com/example/TrinityPrueba/controllers/ClientesController.java
@@ -1,6 +1,9 @@
 package com.example.TrinityPrueba.controllers;
 
+import com.example.TrinityPrueba.dtos.ClienteRequestDto;
+import com.example.TrinityPrueba.dtos.ClienteResponseDto;
 import com.example.TrinityPrueba.entities.Clientes;
+import com.example.TrinityPrueba.mapper.ClienteMapper;
 import com.example.TrinityPrueba.service.ClientesService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,28 +14,42 @@ import java.util.List;
 public class ClientesController {
 
     private final ClientesService clientesService;
+    private final ClienteMapper clienteMapper;
 
-    public ClientesController(ClientesService clientesService) {
+    public ClientesController(ClientesService clientesService, ClienteMapper clienteMapper) {
         this.clientesService = clientesService;
+        this.clienteMapper = clienteMapper;
     }
 
-    @GetMapping("/find")
-    public List<Clientes> getClientes() {
-        return clientesService.getClientes();
+    @GetMapping
+    public List<ClienteResponseDto> getClientes() {
+        List<Clientes> clientes = clientesService.getClientes();
+        return clienteMapper.toResponseList(clientes);
     }
 
-    @PutMapping("/update")
-    public void updateClientes(@RequestBody Clientes clientes) {
-        clientesService.updateClientes(clientes);
+    @GetMapping("/{id}")
+    public ClienteResponseDto getClienteById(@PathVariable Long id) throws Exception {
+        Clientes cliente = clientesService.getClientesByID(id);
+        return clienteMapper.toResponse(cliente);
     }
 
-    @PostMapping("/create")
-    public void createClientes(@RequestBody Clientes clientes) throws Exception {
-        clientesService.createClientes(clientes);
+    @PutMapping("/{id}")
+    public ClienteResponseDto update(@PathVariable long id, @RequestBody ClienteRequestDto dto) throws Exception {
+        Clientes cliente = clienteMapper.toEntity(dto);
+        Clientes updated = clientesService.updateClientes(id, cliente);
+
+        return clienteMapper.toResponse(updated);
     }
 
-    @DeleteMapping("/delete")
-    public void deleteClientes(Clientes clientes) {
-        clientesService.deleteClientes(clientes);
+    @PostMapping
+    public ClienteResponseDto create(@RequestBody ClienteRequestDto dto) throws Exception {
+        Clientes cliente = clienteMapper.toEntity(dto);
+        Clientes saved = clientesService.createClientes(cliente);
+        return clienteMapper.toResponse(saved);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) throws Exception {
+        clientesService.deleteClientes(id);
     }
 }
