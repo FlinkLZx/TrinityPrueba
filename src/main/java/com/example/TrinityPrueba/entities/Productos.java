3b93da087490d2fbe03c3e26f31c90a604d8b7d9
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-22 14:20:12 -0500

    Fixed Productos

--- src/main/java/com/example/TrinityPrueba/entities/Productos.java
+++ src/main/java/com/example/TrinityPrueba/entities/Productos.java
@@ -2,6 +2,9 @@ package com.example.TrinityPrueba.entities;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Entity
 @Table
 public class Productos {
@@ -11,61 +14,76 @@ public class Productos {
     private long Id;
 
     private String tipoDeCuenta;
-
     private String estadoCuenta;
 
-    private String saldo;
-
+    private BigDecimal saldo;
     private Boolean exentaGMF;
 
+    @JoinColumn(name = "cliente_id", nullable = false)
     @ManyToOne
     private Clientes dueño;
+
+    private LocalDateTime creacion;
+    private LocalDateTime modificacion;
 
     public long getId() {
         return Id;
     }
-
     public void setId(long id) {
         Id = id;
     }
 
+
     public String getTipoDeCuenta() {
         return tipoDeCuenta;
     }
-
     public void setTipoDeCuenta(String tipoDeCuenta) {
         this.tipoDeCuenta = tipoDeCuenta;
     }
 
+
     public String getEstadoCuenta() {
         return estadoCuenta;
     }
-
     public void setEstadoCuenta(String estadoCuenta) {
         this.estadoCuenta = estadoCuenta;
     }
 
-    public String getSaldo() {
+
+    public BigDecimal getSaldo() {
         return saldo;
     }
-
-    public void setSaldo(String saldo) {
+    public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
     }
+
 
     public Boolean getExentaGMF() {
         return exentaGMF;
     }
-
     public void setExentaGMF(Boolean exentaGMF) {
         this.exentaGMF = exentaGMF;
     }
 
+
     public Clientes getDueño() {
         return dueño;
     }
-
     public void setDueño(Clientes dueño) {
         this.dueño = dueño;
+    }
+
+    public LocalDateTime getCreacion() {
+        return creacion;
+    }
+    public void setCreacion(LocalDateTime creacion) {
+        this.creacion = creacion;
+    }
+
+    public LocalDateTime getModificacion() {
+        return modificacion;
+    }
+    public void setModificacion(LocalDateTime modificacion) {
+        this.modificacion = modificacion;
     }
 }
