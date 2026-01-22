48f9a71653633f434b98cec6c16e8214adadc0a8
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-22 14:20:59 -0500

    Fixed ProductosRepository

--- src/main/java/com/example/TrinityPrueba/repository/ProductosRepository.java
+++ src/main/java/com/example/TrinityPrueba/repository/ProductosRepository.java
@@ -9,4 +9,6 @@ public interface ProductosRepository extends JpaRepository<Productos, Long> {
 
     boolean existsByDueñoId(Long dueñoId);
 
+    boolean existsByDueñoIdAndExentaGMFTrue(Long clienteId);
+
 }
