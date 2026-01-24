3a00537c94213b1fdcddaf8bff55d9de0fe1fddd
Juan Felipe Artunduaga Quintero <juanfeartunduaga914@gmail.com>
2026-01-24 12:12:36 -0500

    Fixed Productos

--- src/main/java/com/example/TrinityPrueba/entities/Productos.java
+++ src/main/java/com/example/TrinityPrueba/entities/Productos.java
@@ -6,12 +6,12 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table
+@Table(name = "productos")
 public class Productos {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long Id;
+    private long id;
 
     private String tipoDeCuenta;
     private String estadoCuenta;
@@ -19,63 +19,65 @@ public class Productos {
     private BigDecimal saldo;
     private Boolean exentaGMF;
 
-    @JoinColumn(name = "cliente_id", nullable = false)
+    @JoinColumn(nullable = false)
     @ManyToOne
-    private Clientes dueño;
+    private Clientes cliente;
 
     private LocalDateTime creacion;
     private LocalDateTime modificacion;
 
     public long getId() {
-        return Id;
-    }
-    public void setId(long id) {
-        Id = id;
+        return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getTipoDeCuenta() {
         return tipoDeCuenta;
     }
+
     public void setTipoDeCuenta(String tipoDeCuenta) {
         this.tipoDeCuenta = tipoDeCuenta;
     }
 
-
     public String getEstadoCuenta() {
         return estadoCuenta;
     }
+
     public void setEstadoCuenta(String estadoCuenta) {
         this.estadoCuenta = estadoCuenta;
     }
 
-
     public BigDecimal getSaldo() {
         return saldo;
     }
+
     public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
     }
 
-
     public Boolean getExentaGMF() {
         return exentaGMF;
     }
+
     public void setExentaGMF(Boolean exentaGMF) {
         this.exentaGMF = exentaGMF;
     }
 
-
-    public Clientes getDueño() {
-        return dueño;
+    public Clientes getCliente() {
+        return cliente;
     }
-    public void setDueño(Clientes dueño) {
-        this.dueño = dueño;
+
+    public void setCliente(Clientes cliente) {
+        this.cliente = cliente;
     }
 
     public LocalDateTime getCreacion() {
         return creacion;
     }
+
     public void setCreacion(LocalDateTime creacion) {
         this.creacion = creacion;
     }
@@ -83,6 +85,7 @@ public class Productos {
     public LocalDateTime getModificacion() {
         return modificacion;
     }
+
     public void setModificacion(LocalDateTime modificacion) {
         this.modificacion = modificacion;
     }
